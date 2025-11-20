edca1458b742eb43403b5dee5ad262f3308807b8
Brandon <venturajosue711@gmail.com>
2025-11-20 20:04:01 +0000

    Imagenes

--- src/assets/img/imagenes.jsx
+++ src/assets/img/imagenes.jsx
@@ -1,36 +1,59 @@
+import tatooine from "./Tatooine.jpg";
+import Alderan from "./Alderan.webp"
+import Yavin from "./Yavin.webp"
+import Hoth from "./Hoth_SWCT.webp"
+import Dagobah from "./Dagobah.jpeg"
+import Bespin from "./Bespin.webp"
+import Endor from "./Endor.webp"
+import Naboo from "./Naboo.webp"
+import Coruscant from "./Coruscant.webp"
+import Kamino from "./Kamino.webp"
+
+import C3PO from "./C3PO.png"
+import Leia from "./Leia.webp"
+import Owen from "./Owen.webp"
+import Beru from "./Beru.webp"
+import R5D4 from "./R5D4.webp"
+
+import x34 from "./x-34landspeede.jpg"
+import T16 from "./T16.webp"
+import Snowspeeder from "./Snowspeeder.webp"
+import Bomber from "./Bomber.webp"
+
+
 const Images = {
     planets: {
-    "Tatooine": "https://static.wikia.nocookie.net/esstarwars/images/b/b0/Tatooine_TPM.png/revision/latest?cb=20131214162357",
-    "Alderaan": "https://static.wikia.nocookie.net/esstarwars/images/4/4a/Alderaan.jpg/revision/latest?cb=20100723184830",
-    "Yavin IV": "https://static.wikia.nocookie.net/swfanon/images/d/db/Yavin.jpg/revision/latest?cb=20121209023743",
-    "Hoth": "https://static.wikia.nocookie.net/esstarwars/images/d/d1/Hoth.jpg/revision/latest?cb=20060903204554",
-    "Dagobah": "https://static.wikia.nocookie.net/esstarwars/images/1/1c/Dagobah.jpg/revision/latest?cb=20061117132132",
-    "Bespin":"https://static.wikia.nocookie.net/esstarwars/images/2/2c/Bespin_EotECR.png/revision/latest?cb=20170527220537",
-    "Endor":"https://static.wikia.nocookie.net/esstarwars/images/8/83/EndorSpace.jpg/revision/latest?cb=20220414221348",
-    "Naboo":"https://static.wikia.nocookie.net/esstarwars/images/f/f0/Naboo_planet.png/revision/latest?cb=20190928214307",
-    "Coruscant":"https://static.wikia.nocookie.net/esstarwars/images/8/84/CoruscantGlobeE1.png/revision/latest?cb=20221030204600",
-    "Kamino":"https://static.wikia.nocookie.net/esstarwars/images/a/a9/Eaw_Kamino.jpg/revision/latest?cb=20210616005549"
+    "Tatooine": tatooine,
+    "Alderaan": Alderan,
+    "Yavin IV": Yavin,
+    "Hoth": Hoth,
+    "Dagobah": Dagobah,
+    "Bespin":Bespin,
+    "Endor":Endor,
+    "Naboo":Naboo,
+    "Coruscant":Coruscant,
+    "Kamino":Kamino
   }, 
     people: {
       "Luke Skywalker": "https://img.asmedia.epimg.net/resizer/v2/TVLRIW6H75EYPNO3KBHXFTISDA.jpg?auth=1d2278160753689784a2193684678b480f9df18a8fcb8bfe628520b78f5d0547&width=1472&height=828&smart=true",
-      "C-3PO": "https://tooys.mx/media/catalog/product/cache/39c7ff5a74bd9fa282a021db605b774d/c/3/c3po.jpg",
+      "C-3PO": C3PO,
       "R2-D2": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/R2-D2_-_Genuine_Movie_Star.jpg/1200px-R2-D2_-_Genuine_Movie_Star.jpg",
       "Darth Vader": "https://wallpapers.com/images/hd/darth-vader-pictures-qwlyfdkmyjirchwo.jpg",
-      "Leia Organa": "https://static.wikia.nocookie.net/total-movies/images/d/d5/Princess_Leia.jpg/revision/latest?cb=20151105013428",
-      "Owen Lars": "https://static.wikia.nocookie.net/starwars/images/8/81/Owen-OP.jpg/revision/latest?cb=20070626181249",
-      "Beru Whitesun lars": "https://static.wikia.nocookie.net/starwars/images/d/de/BeruWhitesun22BBY-SWCT-Photoroom.png/revision/latest/scale-to-width/360?cb=20241222061726",
-      "R5-D4": "https://static.wikia.nocookie.net/esstarwars/images/3/3f/R5D4-AG.png/revision/latest?cb=20240210190020",
+      "Leia Organa": Leia,
+      "Owen Lars": Owen,
+      "Beru Whitesun lars": Beru,
+      "R5-D4": R5D4,
       "Biggs Darklighter": "https://lumiere-a.akamaihd.net/v1/images/image_606ff7f7.jpeg?region=352%2C0%2C1170%2C878",
       "Obi-Wan Kenobi" : "https://ca-times.brightspotcdn.com/dims4/default/10beaaa/2147483647/strip/true/crop/1263x720+0+0/resize/1200x684!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F57%2F32%2F9f488c2d4bc288bcfce63e749811%2Fobi-wan.JPG"
     },
     vehicles: {
       "Sand Crawler" : "https://m.media-amazon.com/images/I/71ZTESgQfOL.jpg",
-      "X-34 landspeeder" : "https://static.wikia.nocookie.net/starwars/images/b/be/X-34-TGTB.png/revision/latest?cb=20211204185714",
-      "T-16 skyhopper" : "https://static.wikia.nocookie.net/starwars/images/e/e1/Lukes_T-16_Skyhopper_TT.png/revision/latest?cb=20170520044701",
+      "X-34 landspeeder" : x34,
+      "T-16 skyhopper" : T16,
       "TIE/LN starfighter" : "https://diceandcardboard.com/content/images/2022/06/7D447526-4083-44FC-BAAD-FEBA5A3872B0.jpeg",
-      "Snowspeeder" : "https://static.wikia.nocookie.net/starwars/images/0/0d/Snowspeeder.png/revision/latest?cb=20161110010008",
+      "Snowspeeder" : Snowspeeder,
       "AT-AT" : "https://static.posters.cz/image/1300/posters/star-wars-at-at-attack-i67847.jpg",
-      "TIE bomber" : "https://static.wikia.nocookie.net/esstarwars/images/1/17/TIE_Bomber_BF2.png/revision/latest?cb=20171101030957",
+      "TIE bomber" : Bomber,
       "AT-ST" : "https://lumiere-a.akamaihd.net/v1/images/AT-ST-gallery-1_85c9da66.jpeg?region=258,24,751,422",
       "Storm IV Twin-Pod cloud car" : "https://i.pinimg.com/originals/62/e0/0d/62e00d1f888409ac4582f93389444042.jpg",
       "Sail barge" : "https://m.media-amazon.com/images/I/61be0vVXDgL.jpg"
